8742e8d9c2b40d861f271efcdd04076b63a97d40
pearlynlohwq <pearlynlohwq@gmail.com>
2022-02-14 11:29:38 +0800

    adjust margin for colortip

--- lib/src/widget/heatmap_color_tip.dart
+++ lib/src/widget/heatmap_color_tip.dart
@@ -103,7 +103,7 @@ class HeatMapColorTip extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 8.0),
+      padding: const EdgeInsets.only(top: 2.0),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
